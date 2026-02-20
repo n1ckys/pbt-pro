9d2411df2b559c4dab4fc184565dbb9499606a16
n1ckys <nickats.re@gmail.com>
2026-02-20 20:55:14 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "pbt-pro-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch handler
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
